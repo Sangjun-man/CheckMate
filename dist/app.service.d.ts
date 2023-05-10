583bb3aadfe34d5d9ac68be877e50b048d73f80d
Sangjun-man <molung@icloud.com>
2023-05-10 22:11:01 +0900

    fix: ./dist의 빌드 파일 gitignore에 추가

--- dist/app.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Logger } from '@nestjs/common';
-export declare class AppService {
-    private readonly logger;
-    private client;
-    constructor(logger: Logger);
-    private login;
-    getGuildPreview(): Promise<import("discord.js").GuildPreview>;
-}
--- dist/module/csv/csv.model.d.ts
+++
@@ -1,4 +0,0 @@
-export interface CsvModel {
-    createdAt: string;
-    count: number;
-}
